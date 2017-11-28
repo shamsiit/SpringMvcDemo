798ff91aeea6c214eb71026298f566c43b8c41b1
Shahriar Mohammad <shahriar@ipvisionsoft.com>
2017-11-28 11:02:39 +0600

    first commit

---
+++ src/main/java/com/shams/dao/PersonDAO.java
@@ -0,0 +1,15 @@
+package com.shams.dao;
+
+import java.util.List;
+
+import com.shams.model.Person;
+
+
+public interface PersonDAO {
+
+	public void addPerson(Person p);
+	public void updatePerson(Person p);
+	public List<Person> listPersons();
+	public Person getPersonById(int id);
+	public void removePerson(int id);
+}
---
+++ src/main/java/com/shams/model/Person.java
@@ -0,0 +1,55 @@
+package com.shams.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Person")
+public class Person  implements Serializable{
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "country")
+	private String country;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	@Override
+	public String toString() {
+		return "id=" + id + ", name=" + name + ", country=" + country;
+	}
+}
